263d1b82a3f34035e796514e889e508058d4d164
victor <victor_77@naver.com>
2019-02-04 14:56:26 +0900

    bex00 JDBCTest

---
+++ bex00/src/test/java/org/victor/persistence/JDBCTest.java
@@ -0,0 +1,33 @@
+package org.victor.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+
+public class JDBCTest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch( Exception e ) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try( Connection con = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:59161:xe",
+				"system",
+				"oracle")){
+			
+		} catch( Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
+
