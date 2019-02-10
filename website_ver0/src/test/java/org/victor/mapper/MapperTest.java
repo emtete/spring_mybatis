bc8fb7e42bf4c20a4aa38c69d31e4d2135457cfc
victor <victor_77@naver.com>
2019-02-11 01:28:16 +0900

    website_ver0 Mapper_CRUD_Debugging Complete

--- website_ver0/src/test/java/org/victor/mapper/MapperTest.java
+++ website_ver0/src/test/java/org/victor/mapper/MapperTest.java
@@ -42,7 +42,7 @@ public class MapperTest {
 //	@Test
 	public void readTest() {
 		
-		log.info( boardMapper.read(6L) );
+		log.info( boardMapper.read(44L) );
 		System.out.println("readTest Complete");
 		System.out.println("\n\n\n");
 	}
@@ -51,11 +51,10 @@ public class MapperTest {
 //	@Test
 	public void insertTest() {
 		
-		BoardVO board = genBoard(1);
+		BoardVO board = genBoard(3);
 		
 		boardMapper.insert(board);
-		log.info(board);
-		
+		System.out.println("insertTest() : "+board);
 		System.out.println("insertTest Complete");
 		System.out.println("\n\n\n");
 	}
@@ -77,7 +76,7 @@ public class MapperTest {
 	public void updateTest() {
 		
 		BoardVO board = genBoard(2);
-		board.setBno(7L);
+		board.setBno(45L);
 		
 		log.info( boardMapper.update(board) );
 		System.out.println("updateTest Complete");
@@ -88,7 +87,7 @@ public class MapperTest {
 //	@Test
 	public void deleteTest() {
 		
-		log.info( boardMapper.delete(8L) );
+		log.info( boardMapper.delete(46L) );
 		System.out.println("deleteTest Complete");
 		System.out.println("\n\n\n");
 	}
