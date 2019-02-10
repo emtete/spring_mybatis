54a5e624a367c8ed1b7b9737661ca865ed8d99a3
victor <victor_77@naver.com>
2019-02-10 21:48:30 +0900

    website_ver0 Jdbc_Connection_Test

--- website_ver0/src/test/java/org/victor/sample/SampleTests.java
+++ website_ver0/src/test/java/org/victor/sample/SampleTests.java
@@ -8,6 +8,12 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,16 +21,40 @@ import org.junit.runner.RunWith;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
-public class DiTests {
+public class SampleTests {
 
 	@Setter( onMethod_ = @Autowired )
 	private DiParent parent;
 	
-	@Test
+	
+//	@Test
 	public void diTest() {
 		
 		assertNotNull(parent);
 		log.info(parent);
+	}
+	
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		
+		try(Connection con = 
+			DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:59161:xe",
+				"system",
+				"oracle")){
+			
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
 	}
 }
 
